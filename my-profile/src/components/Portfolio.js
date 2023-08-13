37f7c42bedd21e7a668d56c1dd4e460987f1f6a6
hyunahrho29@gmail.com <hyunahrho29@gmail.com>
2023-08-13 22:32:33 +0900

    Update Portfolio.js

--- my-profile/src/components/Portfolio.js
+++ my-profile/src/components/Portfolio.js
@@ -1,8 +1,32 @@
 import "../style/Portfolio.css"
+import title from "../img/title_portfolio.png"
+import port1 from "../img/port1.png"
+import port2 from "../img/port2.png"
+import port3 from "../img/port3.png"
+import port45 from "../img/port45.png"
 
 function Portfolio(){
     return(
-        <div></div>
+        <div className="Portfolio">
+            <div className="Title">
+                <img src={title} alt="Experience" />
+            </div>
+            <div className="Content-Port">
+                <img src={port1} alt="Portfolio"></img>
+            </div>
+            <div className="Content-Port">
+                <img src={port2} alt="Portfolio"></img>
+            </div>
+            <div className="Content-Port">
+                <img src={port3} alt="Portfolio"></img>
+            </div>
+            <div className="Content-Port">
+                <img src={port45} alt="Portfolio"></img>
+            </div>
+            <div className="Content-Port-Add">
+                업데이트중입니다.
+            </div>
+        </div>
     );
 }
 
